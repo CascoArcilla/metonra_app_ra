750cda69f93be68b5dd4f10208022c6c3b598316
CascoArcilla <normon1230@gmail.com>
2025-10-02 12:17:38 -0600

    Correccion de bug en arañas

--- Assets/Scripts/Compresion.cs
+++ Assets/Scripts/Compresion.cs
@@ -3,15 +3,20 @@ using UnityEngine;
 public class Compresion : MonoBehaviour
 {
     [SerializeField] private Transform referencia;
+    [SerializeField] private float distanciaMaxima = 100f;
+    [SerializeField] private float velocidad = 90f;
+    private float distanciaRecorrida = 0f;
 
     void Update()
     {
         if (referencia != null)
         {
-            float distancia = Vector3.Distance(transform.position, referencia.position);
-            if (distancia < 50f)
+            float distanciaActual = Vector3.Distance(transform.position, referencia.position);
+
+            if (distanciaRecorrida <= distanciaMaxima)
             {
-                Destroy(gameObject);
+                Vector3 direccion = (referencia.position - transform.position).normalized;
+                transform.Translate(direccion * velocidad * Time.deltaTime);
             }
         }
     }
